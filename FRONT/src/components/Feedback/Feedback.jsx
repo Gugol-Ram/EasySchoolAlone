a872c7bbaefb649b45651ff604506258f0523765
Ramiro Heredia <94948260+Gugol-Ram@users.noreply.github.com>
2024-05-28 01:23:32 -0300

    feedback form style & updates

--- FRONT/src/components/Feedback/Feedback.jsx
+++ FRONT/src/components/Feedback/Feedback.jsx
@@ -51,107 +51,113 @@ const FeedbackForm = () => {
   };
 
   return (
-    <div className={`container mt-5 ${style.form_container}`}>
-      <h2 className="mb-4">Su opinión es importante.</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>
-            ¿Cómo calificaría la facilidad de uso de nuestra plataforma de
-            inscripción?
-          </label>
-          <select
-            className="form-control"
-            name="easeOfUse"
-            onChange={handleChange}
-            value={feedback.easeOfUse}
-          >
-            <option value="">Seleccionar</option>
-            <option value="Excelente">Excelente</option>
-            <option value="Bueno">Bueno</option>
-            <option value="Regular">Regular</option>
-            <option value="Deficiente">Deficiente</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="satisfaction">
-            En una escala del 1 al 10, ¿cuál es su nivel de satisfacción general
-            con nuestra plataforma?
-          </label>
-          <input
-            value={feedback.satisfaction}
-            onChange={handleChange}
-            type="number"
-            className="form-control"
-            id="satisfaction"
-            min="1"
-            max="10"
-            name="satisfaction"
-          />
-        </div>
+    <div className={style.main_container}>
+      <div className={`container mt-5 ${style.form_container}`}>
+        <h2 className="mb-4">Su opinión es importante.</h2>
+        <form onSubmit={handleSubmit}>
+          <div className={`form-group ${style.form_group}`}>
+            <label>
+              ¿Cómo calificaría la facilidad de uso de nuestra plataforma de
+              inscripción?
+            </label>
+            <select
+              className="form-control"
+              name="easeOfUse"
+              onChange={handleChange}
+              value={feedback.easeOfUse}
+            >
+              <option value="">Seleccionar</option>
+              <option value="Excelente">Excelente</option>
+              <option value="Bueno">Bueno</option>
+              <option value="Regular">Regular</option>
+              <option value="Deficiente">Deficiente</option>
+            </select>
+          </div>
+          <div className={`form-group ${style.form_group}`}>
+            <label htmlFor="satisfaction">
+              En una escala del 1 al 10, ¿cuál es su nivel de satisfacción
+              general con nuestra plataforma?
+            </label>
+            <input
+              value={feedback.satisfaction}
+              onChange={handleChange}
+              type="number"
+              className="form-control"
+              id="satisfaction"
+              min="1"
+              max="10"
+              name="satisfaction"
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="registrationProcess">
-            ¿Cómo fue su experiencia durante el proceso de registro de padres y
-            estudiantes?
-          </label>
-          <textarea
-            onChange={handleChange}
-            className="form-control"
-            id="registrationProcess"
-            rows="3"
-            name="registrationProcess"
-            value={feedback.registrationProcess}
-          ></textarea>
-        </div>
-        <div className="form-group">
-          <label htmlFor="userInterface">
-            ¿Cómo calificaría la apariencia y la interfaz de usuario de la
-            plataforma?
-          </label>
-          <textarea
-            onChange={handleChange}
-            className="form-control"
-            id="userInterface"
-            name="userInterface"
-            rows="3"
-            value={feedback.userInterface}
-          ></textarea>
-        </div>
+          <div className={`form-group ${style.form_group}`}>
+            <label htmlFor="registrationProcess">
+              ¿Cómo fue su experiencia durante el proceso de registro de padres
+              y estudiantes?
+            </label>
+            <textarea
+              onChange={handleChange}
+              className="form-control"
+              id="registrationProcess"
+              rows="3"
+              name="registrationProcess"
+              value={feedback.registrationProcess}
+            ></textarea>
+          </div>
+          <div className={`form-group ${style.form_group}`}>
+            <label htmlFor="userInterface">
+              ¿Cómo calificaría la apariencia y la interfaz de usuario de la
+              plataforma?
+            </label>
+            <textarea
+              onChange={handleChange}
+              className="form-control"
+              id="userInterface"
+              name="userInterface"
+              rows="3"
+              value={feedback.userInterface}
+            ></textarea>
+          </div>
 
-        <div className="form-group">
-          <label>¿Recomendaría nuestra plataforma a otros padres?</label>
-          <select
-            className="form-control"
-            name="recommendation"
-            onChange={handleChange}
-            value={feedback.recommendation}
-            id="recommendation"
-          >
-            <option value="">Seleccionar</option>
-            <option value="true">Si</option>
-            <option value="false">No</option>
-          </select>
-        </div>
+          <div className={`form-group ${style.form_group}`}>
+            <label>¿Recomendaría nuestra plataforma a otros padres?</label>
+            <select
+              className="form-control"
+              name="recommendation"
+              onChange={handleChange}
+              value={feedback.recommendation}
+              id="recommendation"
+            >
+              <option value="">Seleccionar</option>
+              <option value="true">Si</option>
+              <option value="false">No</option>
+            </select>
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="additionalComments">
-            Comentarios adicionales o sugerencias
-          </label>
-          <textarea
-            onChange={handleChange}
-            className="form-control"
-            id="additionalComments"
-            name="additionalComments"
-            rows="3"
-            value={feedback.additionalComments}
-          ></textarea>
-        </div>
-        <div className="text-center">
-          <button type="submit" className="btn btn-primary">
-            Enviar
-          </button>
-        </div>
-      </form>
+          <div className={`form-group ${style.form_group}`}>
+            <label htmlFor="additionalComments">
+              Comentarios adicionales o sugerencias
+            </label>
+            <textarea
+              onChange={handleChange}
+              className="form-control"
+              id="additionalComments"
+              name="additionalComments"
+              rows="3"
+              value={feedback.additionalComments}
+            ></textarea>
+            <br />
+          </div>
+          <div className={`text-center ${style.text_center}`}>
+            <button
+              type="submit"
+              className={`btn btn-primary ${style.btn_primary}`}
+            >
+              Enviar
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
