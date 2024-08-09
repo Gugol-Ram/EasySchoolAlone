382fc5e2c5d722c584edaa418f3796997e7436f4
Ramiro Heredia <94948260+Gugol-Ram@users.noreply.github.com>
2024-08-09 02:08:52 -0300

    update data loading when recently login

--- FRONT/src/components/ViewParent/MainComponents/MyProfile/MyProfile.jsx
+++ FRONT/src/components/ViewParent/MainComponents/MyProfile/MyProfile.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap"; // Importa Spinner
 import style from "./myProfile.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getParent } from "../../../../redux/actions/actions-parents";
 
 function MyProfile() {
+  const [loading, setLoading] = useState(true); // Estado de carga
   const dispatch = useDispatch();
   const parentId = useSelector((state) => state.parentId);
   const parent = useSelector((state) => state.parent);
@@ -13,10 +14,24 @@ function MyProfile() {
 
   useEffect(() => {
     if (filteredParentId) {
-      dispatch(getParent(filteredParentId));
+      dispatch(getParent(filteredParentId))
+        .then(() => setLoading(false)) // Termina la carga cuando los datos se obtienen
+        .catch(() => setLoading(false)); // Asegúrate de manejar errores y terminar la carga
       sessionStorage.setItem("parentId", filteredParentId);
+    } else {
+      setLoading(false); // Termina la carga si no hay parentId
     }
   }, [dispatch, filteredParentId]);
+
+  if (loading) {
+    return (
+      <div className={style.spinnerContainer}>
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Cargando...</span>
+        </Spinner>
+      </div>
+    );
+  }
 
   return (
     <div className={style.main_container}>
@@ -73,8 +88,8 @@ function MyProfile() {
               <br />
               <p className={style.notRegisteredMsg}>
                 Una vez que hayas llenado tus datos podrás interactuar de una
-                manera más completa con la página, como proceder a la inscrpción
-                del Estudiante y/o dejarnos tu opinión.
+                manera más completa con la página, como proceder a la
+                inscripción del Estudiante y/o dejarnos tu opinión.
               </p>
 
               <Button
@@ -104,3 +119,110 @@ function MyProfile() {
 }
 
 export default MyProfile;
+
+// import { NavLink } from "react-router-dom";
+// import { Button } from "react-bootstrap";
+// import style from "./myProfile.module.css";
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { getParent } from "../../../../redux/actions/actions-parents";
+
+// function MyProfile() {
+//   const dispatch = useDispatch();
+//   const parentId = useSelector((state) => state.parentId);
+//   const parent = useSelector((state) => state.parent);
+//   const filteredParentId = parentId?.parentDetails?.[0]?.id;
+
+//   useEffect(() => {
+//     if (filteredParentId) {
+//       dispatch(getParent(filteredParentId));
+//       sessionStorage.setItem("parentId", filteredParentId);
+//     }
+//   }, [dispatch, filteredParentId]);
+
+//   return (
+//     <div className={style.main_container}>
+//       <h2 className={style.title}>Datos personales</h2>
+
+//       <br />
+//       <hr />
+//       <br />
+//       <div>
+//         <br />
+//         <div>
+//           {parentId.parentDetails && parentId.parentDetails.length > 0 ? (
+//             <div className={style.profile_container}>
+//               <img src={parent.fotoDocumento} className={style.docImage}></img>
+//               <p>
+//                 <strong>Nombre:</strong> {parent.name}
+//               </p>
+//               <p>
+//                 <strong>Apellido:</strong> {parent.lastName}
+//               </p>
+//               <p>
+//                 <strong>Documento:</strong> {parent.idDoc}
+//               </p>
+//               <p>
+//                 <strong>Email:</strong> {parent.email}
+//               </p>
+//               <p>
+//                 <strong>Teléfono personal:</strong> {parent.contactCellphone}
+//               </p>
+//               <p>
+//                 <strong>Teléfono del lugar de trabajo:</strong>{" "}
+//                 {parent.jobTelephone}
+//               </p>
+//               <p>
+//                 <strong>Domicilio:</strong> {parent.address}
+//               </p>
+//               <p>
+//                 <strong>Dirección del trabajo:</strong> {parent.jobAddress}
+//               </p>
+//               {!parent.state && (
+//                 <p>
+//                   <strong>
+//                     El padre se encuentra suspendido temporalmente, para más
+//                     información contactar al administrador escolar.
+//                   </strong>
+//                 </p>
+//               )}
+//             </div>
+//           ) : (
+//             <div className={style.notRegistered}>
+//               <h5>
+//                 <strong>Aún no te has registrado como padre.</strong>
+//               </h5>
+//               <br />
+//               <p className={style.notRegisteredMsg}>
+//                 Una vez que hayas llenado tus datos podrás interactuar de una
+//                 manera más completa con la página, como proceder a la inscrpción
+//                 del Estudiante y/o dejarnos tu opinión.
+//               </p>
+
+//               <Button
+//                 as={NavLink}
+//                 to={"/formParent"}
+//                 className={style.link_to_parent}
+//               >
+//                 Llenar datos
+//               </Button>
+//             </div>
+//           )}
+//           <br />
+//           {parentId.parentDetails &&
+//             parentId.parentDetails.length > 0 &&
+//             !parent.validate && (
+//               <div className={style.validation_message}>
+//                 <h5>
+//                   Su perfil está en proceso de validación; una vez el
+//                   administrador lo valide, podrá inscribir a sus estudiantes.
+//                 </h5>
+//               </div>
+//             )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default MyProfile;
